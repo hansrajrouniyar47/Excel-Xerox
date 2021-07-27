b54b6fe0467a1564dc2b585b333577a8096a515f
hansrajrouniyar47 <hansrajrouniyar47@gmail.com>
2021-07-27 17:38:27 +0530

    committing

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let ans="";
-let n=25;
-while(n>0){
-  let rem=n%26;
-  if(rem==0){
-    ans="Z"+ans;
-    n=Math.floor(n/26)-1;
-  }
-  else{
-    ans=String.fromCharCode(rem-1+65)+ans;
-    n=Math.floor(n/26);
-  }
-}
-console.log(ans);
+$(document).ready(function(){
+    let cellContainer=$(".input-cell-container");
+
+    for(let i=1;i<=100;i++){
+      let ans="";
+
+      let n=i;
+
+      while(n>0){
+        let rem=n%26;
+        if(rem==0){
+          ans="Z"+ans;
+          n=Math.floor(n/26)-1;
+        }
+        else{
+          ans=String.fromCharCode(rem-1+65)+ans;
+          n=Math.floor(n/26);
+        }
+      }
+
+      let column=$(`div class="column-name" id="colCod-${ans} colId-${i}">${ans}</div>`);
+      $(".column-name-container").append(column);
+      let row=$(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+      $(".row-name-container").append(row);
+
+
+    }
+
+});
+
+
+
